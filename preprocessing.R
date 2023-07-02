1b7b5d4b81fe7a14dc43d0fd0d98638866494968
OzMashiah <ozmashiah10@gmail.com>
2023-07-02 20:38:14 +0300

    starting to read the raw data for the preprocess

---
+++ preprocessing.R
@@ -0,0 +1,10 @@
+rm(list = ls()) # remove all variables from environment
+cat("\014") # clean console
+# import libraries
+library(readr)
+
+# read raw data files
+beginning<-read_tsv('rawdata\\Beginning Survey_July 2, 2023_04.49.tsv', locale = locale(encoding = "UTF-16"))
+ending<-readr::read_tsv("rawdata\\Ending Survey_July 2, 2023_04.49.tsv", locale = locale(encoding = "UTF-16"))
+daily<-readr::read_tsv("rawdata\\daily survey_July 2, 2023_04.48.tsv", locale = locale(encoding = "UTF-16"))
+
