d36de7fde1cf979ce98baa7532dcf99524cf6447
OzMashiah <ozmashiah10@gmail.com>
2023-07-03 14:06:36 +0300

    remove duplicates and did not finish

--- preprocessing.R
+++ preprocessing.R
@@ -3,8 +3,12 @@ cat("\014") # clean console
 # import libraries
 library(readr)
 
+#set working directory
+setwd("C:/Users/Oz/Documents/Empathy And Acceptance/empathy-and-acceptance/")
+
 # read raw data files
 beginning<-read_tsv('rawdata\\Beginning Survey_July 2, 2023_04.49.tsv', locale = locale(encoding = "UTF-16"))
 ending<-readr::read_tsv("rawdata\\Ending Survey_July 2, 2023_04.49.tsv", locale = locale(encoding = "UTF-16"))
 daily<-readr::read_tsv("rawdata\\daily survey_July 2, 2023_04.48.tsv", locale = locale(encoding = "UTF-16"))
 
+#
